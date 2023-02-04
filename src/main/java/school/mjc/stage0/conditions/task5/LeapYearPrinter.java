3316fa8a17aa62aed16a2c3e061c9cbe98e06a4b
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-02-04 19:57:24 +0100

    completed

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,30 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch(year%400)
+        {
+            case 0:
+                System.out.println("Leap year");
+                break;
+            default:
+                switch(year%4 )
+                {
+                    case 0:
+                        switch(year%100)
+                        {
+                            case 0:
+                                System.out.println("Not a leap year");
+                                break;
+                            default:
+                                System.out.println("leap year");
+                                break;
+                        }
+                        break;
+                    default:
+                        System.out.println("Not a leap year");
+                        break;
+                }
+                break;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,32 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        String season;
+        switch (month) {
+            case (1):
+            case (2):
+            case (12):
+                season = "winter";
+                break;
+            case (3):
+            case (4):
+            case (5):
+                season = "spring";
+                break;
+            case (6):
+            case (7):
+            case (8):
+                season = "summer";
+                break;
+            case (9):
+            case (10):
+            case (11):
+                season = "autumn";
+                break;
+            default:
+                season = "invalid number of month!";
+                break;
+        }
+        System.out.println(season);
     }
 }
